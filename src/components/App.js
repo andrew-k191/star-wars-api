7c60b7d540d6f860518c1494b115f826ea3b5cc3
Andrew Knight <knightandrew21@icloud.com>
2021-10-13 14:01:07 -0600

    working on making the HTTP request

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,30 @@
 import React from 'react';
 import StarWarsTable from './StarWarsTable';
+import axios from 'axios';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      // this state should keep track of characters information, since 10 is the maximum,
+      CharacterData: {
+        name: '',
+      },
+    };
+  }
+
+  // We need to make an HTTP request using Axios here
+  componentDidMount() {
+    axios
+      .get('https://swapi.dev/api/people')
+      .then((response) => {
+        console.log(response.data.results);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
   render() {
     return (
       <div>
